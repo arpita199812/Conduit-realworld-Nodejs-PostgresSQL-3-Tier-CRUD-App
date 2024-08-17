97abf2b34d8371307636b4eea92fe0978aee9be9
Arpita Singh <158558110+arpita199812@users.noreply.github.com>
2024-08-17 13:04:00 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,11 +4,15 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0',  // Allow access from external IPs
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:3001'
+        target: 'http://localhost:3001',
+        changeOrigin: true,  // Ensure the host header is changed to the target
+        secure: false,  // Allow proxying to servers with self-signed certificates
       }
     }
   }
 })
+
