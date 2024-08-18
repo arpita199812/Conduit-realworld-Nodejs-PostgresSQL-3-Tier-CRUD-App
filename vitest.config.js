a016c298d45559cba0c9bf93a9aa72d7a7994214
Arpita Singh <158558110+arpita199812@users.noreply.github.com>
2024-08-18 14:36:41 +0530

    Update vitest.config.js

--- vitest.config.js
+++ vitest.config.js
@@ -4,14 +4,14 @@ import react from '@vitejs/plugin-react-swc';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:3001', // Ensure this matches your backend URL
+        target: 'http://localhost:3001',
         changeOrigin: true,
-        secure: false, // If you are using HTTPS on the backend, set this to true
+        secure: false,
       }
     }
   }
 });
+
 
