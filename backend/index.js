eb5da0b5f51801188e099c98b9a394f5208b0e7e
Arpita Singh <158558110+arpita199812@users.noreply.github.com>
2024-08-18 14:47:15 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,4 @@
-require('dotenv').config(); // Load environment variables from .env file
+require("dotenv").config();
 const jwtSecret = process.env.JWT_SECRET;
 const env = process.env.NODE_ENV || "development";
 const PORT = process.env.PORT || 3001;
@@ -13,43 +13,34 @@ const articlesRoutes = require("./routes/articles");
 const profilesRoutes = require("./routes/profiles");
 const tagsRoutes = require("./routes/tags");
 
-// Remove or comment out this line if you don't have the file
-// const protectedRoutes = require('./routes/protectedRoutes');
-
 const app = express();
 app.use(cors());
 app.use(express.json());
-
-// Serve static files in production
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("../frontend/dist"));
-} else {
-  app.get("/", (req, res) => res.json({ status: "API is running on /api" }));
-}
-
-app.use("/api/users", usersRoutes);
-app.use("/api/user", userRoutes);
-app.use("/api/articles", articlesRoutes);
-app.use("/api/profiles", profilesRoutes);
-app.use("/api/tags", tagsRoutes);
-
-// Remove or comment out this line if you don't have the file
-// app.use('/api/protected', protectedRoutes);
-
-app.get("*", (req, res) =>
-  res.status(404).json({ errors: { body: ["Not found"] } }),
-);
-
-app.use(errorHandler);
 
 (async () => {
   try {
     await sequelize.sync({ alter: true });
     console.log(`Connection with ${env} database has been established.`);
-    app.listen(PORT, () =>
-      console.log(`Server running on http://localhost:${PORT}`)
-    );
   } catch (error) {
     console.error("Unable to connect to the database:", error);
   }
 })();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("../frontend/dist"));
+} else {
+  app.get("/", (req, res) => res.json({ status: "API is running on /api" }));
+}
+app.use("/api/users", usersRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/articles", articlesRoutes);
+app.use("/api/profiles", profilesRoutes);
+app.use("/api/tags", tagsRoutes);
+app.get("*", (req, res) =>
+  res.status(404).json({ errors: { body: ["Not found"] } }),
+);
+app.use(errorHandler);
+
+app.listen(PORT, () =>
+  console.log(`Server running on http://localhost:${PORT}`),
+);
