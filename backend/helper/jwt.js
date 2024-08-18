699f58f5dccfa2c7349cdbc41aae24f1907e2044
Arpita Singh <158558110+arpita199812@users.noreply.github.com>
2024-08-18 15:51:18 +0530

    Update jwt.js

--- backend/helper/jwt.js
+++ backend/helper/jwt.js
@@ -1,17 +1,18 @@
-const jwt = require('jsonwebtoken');
-const jwtSecret = process.env.JWT_KEY; // Use JWT_KEY environment variable
+const jwt = require("jsonwebtoken");
+const privateKey = process.env.JWT_KEY;
 
-if (!jwtSecret) {
+if (!privateKey) {
+  console.error('JWT_KEY environment variable is not set');
   throw new Error('JWT_KEY environment variable is not set');
 }
 
 module.exports.jwtSign = async (payload) => {
   return jwt.sign(
     { username: payload.username, email: payload.email },
-    jwtSecret, // Use jwtSecret here
+    privateKey
   );
 };
 
 module.exports.jwtVerify = async (token) => {
-  return jwt.verify(token, jwtSecret); // Use jwtSecret here
+  return jwt.verify(token, privateKey);
 };
