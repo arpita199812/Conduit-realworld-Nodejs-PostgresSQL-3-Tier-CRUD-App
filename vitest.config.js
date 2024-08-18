4d926c0dad1102bc8ba22b4e12d6889c98aa05f2
Arpita Singh <158558110+arpita199812@users.noreply.github.com>
2024-08-18 14:31:42 +0530

    Update vitest.config.js

--- vitest.config.js
+++ vitest.config.js
@@ -1,12 +1,17 @@
-import react from "@vitejs/plugin-react-swc";
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
-  test: {
-    globals: true,
-    environment: "jsdom",
-    setupFiles: "frontend/src/setupTests.js",
-    css: true,
-  },
+  server: {
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001', // Ensure this matches your backend URL
+        changeOrigin: true,
+        secure: false, // If you are using HTTPS on the backend, set this to true
+      }
+    }
+  }
 });
+
