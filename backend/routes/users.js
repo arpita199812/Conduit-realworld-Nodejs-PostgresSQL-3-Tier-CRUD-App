dede6b692187fb1b9482ac7ab3a7a3ce7eba648b
Arpita Singh <158558110+arpita199812@users.noreply.github.com>
2024-08-18 14:18:39 +0530

    Update users.js

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,59 +1,10 @@
-const { jwtSign } = require("../helper/jwt");
-const { User } = require("../models");
-const { bcryptHash, bcryptCompare } = require("../helper/bcrypt");
-const {
-  ValidationError,
-  FieldRequiredError,
-  AlreadyTakenError,
-  NotFoundError,
-} = require("../helper/customErrors");
+const express = require("express");
+const router = express.Router();
+const { signUp, signIn } = require("../controllers/users");
 
 // Register
-const signUp = async (req, res, next) => {
-  try {
-    const { username, email, bio, image, password } = req.body.user;
-    if (!username) throw new FieldRequiredError(`A username`);
-    if (!email) throw new FieldRequiredError(`An email`);
-    if (!password) throw new FieldRequiredError(`A password`);
-
-    const userExists = await User.findOne({
-      where: { email: req.body.user.email },
-    });
-    if (userExists) throw new AlreadyTakenError("Email", "try logging in");
-
-    const newUser = await User.create({
-      email: email,
-      username: username,
-      bio: bio,
-      image: image,
-      password: await bcryptHash(password),
-    });
-
-    newUser.dataValues.token = await jwtSign(newUser);
-
-    res.status(201).json({ user: newUser });
-  } catch (error) {
-    next(error);
-  }
-};
-
+router.post("/", signUp);
 // Login
-const signIn = async (req, res, next) => {
-  try {
-    const { user } = req.body;
+router.post("/login", signIn);
 
-    const existentUser = await User.findOne({ where: { email: user.email } });
-    if (!existentUser) throw new NotFoundError("Email", "sign in first");
-
-    const pwd = await bcryptCompare(user.password, existentUser.password);
-    if (!pwd) throw new ValidationError("Wrong email/password combination");
-
-    existentUser.dataValues.token = await jwtSign(user);
-
-    res.json({ user: existentUser });
-  } catch (error) {
-    next(error);
-  }
-};
-
-module.exports = { signUp, signIn };
+module.exports = router;
