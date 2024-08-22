0a1ca6f20d59a5014faf0741961e25c710584d79
Arpita Singh <158558110+arpita199812@users.noreply.github.com>
2024-08-22 17:40:55 +0530

    Update config.js

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,10 +1,24 @@
 /** @type {import('sequelize').Options} */
 module.exports = {
-    development: {
+   development: {
     username: 'root',  // Your database username
     password: 'password123',  // Your database password
     database: 'ht',  // Your database name
-    host: 'ht.clikieeiijdc.us-east-1.rds.amazonaws.com:3306',  // Add the missing comma here
+    host: 'ht.clikieeiijdc.us-east-1.rds.amazonaws.com',  // Add the missing comma here
     dialect: 'mysql'  // Correctly defined dialect
+  },
+   test: {
+    username: 'root',  // Your database username
+    password: 'password123',  // Your database password
+    database: 'ht',  // Your test database name
+    host: 'ht.clikieeiijdc.us-east-1.rds.amazonaws.com',  // Your test database host
+    dialect: 'mysql',  // Specify the dialect directl
+  },
+  production: {
+    username: 'root',  // Your database username
+    password: 'password123',  // Your database password
+    database: 'ht',  // Your production database name
+    host: 'ht.clikieeiijdc.us-east-1.rds.amazonaws.com',  // Your production database host
+    dialect: 'mysql',  // Specify the dialect directly
   },
 };
