6c648984cd36aca81a7efdfb7d1fb5b7bbc597da
Arpita Singh <158558110+arpita199812@users.noreply.github.com>
2024-08-22 17:31:49 +0530

    Update config.js

--- backend/config/config.js
+++ backend/config/config.js
@@ -4,7 +4,7 @@ module.exports = {
     username: 'root',  // Your database username
     password: 'password123',  // Your database password
     database: 'ht',  // Your database name
-    host: 'ht.clikieeiijdc.us-east-1.rds.amazonaws.com:3306'
-    dialect: 'mysql'
+    host: 'ht.clikieeiijdc.us-east-1.rds.amazonaws.com:3306',  // Add the missing comma here
+    dialect: 'mysql'  // Correctly defined dialect
   },
 };
