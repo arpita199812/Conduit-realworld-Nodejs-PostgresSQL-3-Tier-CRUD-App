0626f15c11dc988791ac513542e602169506e7c7
Arpita Singh <158558110+arpita199812@users.noreply.github.com>
2024-08-22 17:16:28 +0530

    Update config.js

--- backend/config/config.js
+++ backend/config/config.js
@@ -5,23 +5,4 @@ module.exports = {
     password: 'password123',  // Your database password
     database: 'ht',  // Your database name
     host: 'ht.clikieeiijdc.us-east-1.rds.amazonaws.com:3306'
-  },
-  #test: {
-    username: 'root',  // Your database username
-    password: 'root',  // Your database password
-    database: 'database_testing',  // Your test database name
-    host: '127.0.0.1',  // Your test database host
-    dialect: 'mysql',  // Specify the dialect directly
-    logging: true,  // Optional: Set to false to disable SQL query logging
-    jwtKey: process.env.JWT_KEY,  // JWT key for test
-  },
-  #production: {
-    username: 'root',  // Your database username
-    password: 'root',  // Your database password
-    database: 'database_production',  // Your production database name
-    host: '127.0.0.1',  // Your production database host
-    dialect: 'mysql',  // Specify the dialect directly
-    logging: false,  // Optional: Set to false to disable SQL query logging
-    jwtKey: process.env.JWT_KEY,  // JWT key for production
-  },
-};
+  }
