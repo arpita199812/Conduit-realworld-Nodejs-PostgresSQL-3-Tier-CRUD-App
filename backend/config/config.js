7faefc9f9aeabdb82573ca583df02848cf25f841
Arpita Singh <158558110+arpita199812@users.noreply.github.com>
2024-08-22 17:26:23 +0530

    Update config.js

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,10 +1,10 @@
 /** @type {import('sequelize').Options} */
 module.exports = {
-   development: {
+   ht: {
     username: 'root',  // Your database username
     password: 'password123',  // Your database password
     database: 'ht',  // Your database name
     host: 'ht.clikieeiijdc.us-east-1.rds.amazonaws.com:3306'
-    dialect: 'mysql'
+    dialect: process.env.DB_DIALECT
   },
 };
